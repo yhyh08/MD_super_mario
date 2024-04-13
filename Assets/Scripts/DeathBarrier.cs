42c03f884b4e3ba6923857ff81e835b0df4d0cbc
yhyh08 <yihhuei@marslab.com.my>
2024-04-14 00:12:36 +0800

    delete file

--- Assets/Scripts/DeathBarrier.cs
+++ Assets/Scripts/DeathBarrier.cs
@@ -5,25 +5,10 @@ using UnityEngine.UI;
 [RequireComponent(typeof(BoxCollider2D))]
 public class DeathBarrier : MonoBehaviour
 {
-    //public AudioSource deathSource;
-    //public AudioClip deathClip;
-    //[SerializeField] SoundManager soundManager;
-    //[SerializeField] AudioClip deathClip;
-
-    private void Start()
-    {
-        //deathSource = GetComponent<AudioSource>();
-        //deathSource.clip = deathClip;
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            // Play the death sound effect
-            //deathSource.Play();
-            //soundManager.PlaySoundFX(deathClip);
-
             // Deactivate the player GameObject
             other.gameObject.SetActive(false);
             
